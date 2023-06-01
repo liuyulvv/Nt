1df6854ec8a7129ee05f113d68a665c07031dad5
liuyulvv <liuyulvv@outlook.com>
2023-06-01 18:07:17 +0800

    add: NPosition

---
+++ NtCore/include/NPosition.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * @file NPosition.h
+ * @author liuyulvv (liuyulvv@outlook.com)
+ * @date 2023-05-31
+ */
+
+#include <cstdint>
+
+struct NPosition {
+    int32_t x_{0};
+    int32_t y_{0};
+};
