90f7c3e3079c51e02e74189f4be968f432dca5ef
liuyulvv <liuyulvv@outlook.com>
2023-06-01 18:07:09 +0800

    add: NSize

---
+++ NtCore/include/NSize.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * @file NSize.h
+ * @author liuyulvv (liuyulvv@outlook.com)
+ * @date 2023-05-31
+ */
+
+#include <cstdint>
+
+struct NSize {
+    uint32_t width_{0};
+    uint32_t height_{0};
+};
